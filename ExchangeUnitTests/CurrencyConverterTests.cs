b5f7ae923fd672aa82a4cdf4d083a31b5a3b0044
Alex Kavaleuski <alexey.kavaleuski@vexcash.com>
2025-01-13 12:54:11 +0200

    ass Mock test case

--- ExchangeUnitTests/CurrencyConverterTests.cs
+++ ExchangeUnitTests/CurrencyConverterTests.cs
@@ -1,6 +1,8 @@
 using Exchange;
 using Exchange.Enums;
+using Exchange.Interfaces;
 using Exchange.Models;
+using Moq;
 
 namespace ExchangeUnitTests;
 
@@ -13,6 +15,25 @@ public class CurrencyConverterTests
         var currencyProvider = new HardcodedCurrencyProvider();
         var currencyConverter = new CurrencyConverter(currencyProvider);
 
+        var pair = new CurrencyPair(Currency.EUR, Currency.DKK);
+
+        // Act
+        decimal result = currencyConverter.Convert(pair, 100);
+
+        // Assert
+        Assert.Equal(743.94m, result);
+    }
+
+    [Fact]
+    public void Convert_WithValidInput_Succeeds_WithMock()
+    {
+        // Arrange
+        var mockProvider = new Mock<ICurrencyProvider>();
+        mockProvider
+            .Setup(p => p.GetExchangeRate(It.IsAny<CurrencyPair>()))
+            .Returns(7.4394m);
+
+        var currencyConverter = new CurrencyConverter(mockProvider.Object);
         var pair = new CurrencyPair(Currency.EUR, Currency.DKK);
 
         // Act
